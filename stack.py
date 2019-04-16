27545ea3d2811e7d97b570f486eb5435c1f25878
SarinSwift <sarinyaswift@gmail.com>
2019-04-16 13:39:23 -0700

    completed stacks implementation on list

---
+++ stack.py
@@ -0,0 +1,99 @@
+#!python
+
+from linkedlist import LinkedList
+
+
+# Implement LinkedStack below, then change the assignment at the bottom
+# to use this Stack implementation to verify it passes all tests
+class LinkedStack(object):
+
+    def __init__(self, iterable=None):
+        """Initialize this stack and push the given items, if any."""
+        # Initialize a new linked list to store the items
+        self.list = LinkedList()
+        if iterable is not None:
+            for item in iterable:
+                self.push(item)
+
+    def __repr__(self):
+        """Return a string representation of this stack."""
+        return 'Stack({} items, top={})'.format(self.length(), self.peek())
+
+    def is_empty(self):
+        """Return True if this stack is empty, or False otherwise."""
+        # TODO: Check if empty
+
+    def length(self):
+        """Return the number of items in this stack."""
+        # TODO: Count number of items
+
+    def push(self, item):
+        """Insert the given item on the top of this stack.
+        Running time: O(???) – Why? [TODO]"""
+        # TODO: Push given item
+
+    def peek(self):
+        """Return the item on the top of this stack without removing it,
+        or None if this stack is empty."""
+        # TODO: Return top item, if any
+
+    def pop(self):
+        """Remove and return the item on the top of this stack,
+        or raise ValueError if this stack is empty.
+        Running time: O(???) – Why? [TODO]"""
+        # TODO: Remove and return top item, if any
+
+
+# Implement ArrayStack below, then change the assignment at the bottom
+# to use this Stack implementation to verify it passes all tests
+class ArrayStack(object):
+
+    def __init__(self, iterable=None):
+        """Initialize this stack and push the given items, if any."""
+        # Initialize a new list (dynamic array) to store the items
+        self.list = list()
+        if iterable is not None:
+            for item in iterable:
+                self.push(item)
+
+    def __repr__(self):
+        """Return a string representation of this stack."""
+        return 'Stack({} items, top={})'.format(self.length(), self.peek())
+
+    def is_empty(self):
+        """Return True if this stack is empty, or False otherwise."""
+        if len(self.list) == 0:
+            return True
+        return False
+
+    def length(self):
+        """Return the number of items in this stack."""
+        return len(self.list)
+
+    def push(self, item):       # adding to last item in array
+        """Insert the given item on the top of this stack.
+        Run time: O(1) append takes constant runtime"""
+        self.list.append(item)
+
+    def peek(self):             # returning last item in list
+        """Return the item on the top of this stack without removing it,
+        or None if this stack is empty."""
+        if not self.list:       # checks if the list is empty
+            return None
+        return self.list[-1]
+
+
+    def pop(self):
+        """Remove and return the item on the top of this stack,
+        or raise ValueError if this stack is empty.
+        Running time: O(1) pop method is constant runtime"""
+        if self.list:           # list is not empty
+            return self.list.pop(len(self.list) - 1)
+        raise ValueError('No items in list to pop: []')
+
+
+
+# Implement LinkedStack and ArrayStack above, then change the assignment below
+# to use each of your Stack implementations to verify they each pass all tests
+# Stack = LinkedStack
+Stack = ArrayStack
